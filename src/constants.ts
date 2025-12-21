ca34d579bc8bd62048b2e92fb416bfd3a8ef12fa
Yusuke Kawamoto <novogrammer@gmail.com>
2025-12-21 12:36:13 +0900

    イテレーションステップの最大値が大きすぎたので調整

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,7 @@ export const SAND_SIMULATOR_HEIGHT=128*4;
 // 60の倍数
 export const ITERATION_PER_SEC=60*4;
 
-export const ITERATION_PER_STEP_MAX=100;
+export const ITERATION_PER_STEP_MAX=8;
 export const CAPTURE_CYCLE_DURATION=5;
 export const SAND_TTL_MIN=100;
 export const SAND_TTL_MAX=200;
